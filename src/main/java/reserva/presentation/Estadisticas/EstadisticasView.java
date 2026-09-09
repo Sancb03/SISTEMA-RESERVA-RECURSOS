a55197d358fa155f5cb2fbbde0d137e27e861d2e
Mariangel <marycorderopicado03@gmail.com>
2026-09-09 16:24:11 -0600

    interfaz estadisticas

--- src/main/java/reserva/presentation/Estadisticas/EstadisticasView.java
+++ src/main/java/reserva/presentation/Estadisticas/EstadisticasView.java
@@ -43,5 +43,7 @@ public class EstadisticasView {
 
     public JTable getEstadisticasActtable(){ return EstadisticasActtable; }
 
+    public JPanel getGraficoRecPanel(){ return GraficoRecPanel; }
 
+    public JPanel getGraficoActPanel(){ return GraficoActPanel; }
 }
